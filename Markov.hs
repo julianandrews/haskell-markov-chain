18b6d42ec6d524e1719301962f543388471f485d
Julian Andrews <jandrews@fusionbox.com>
2016-03-13 08:21:37 -0600

    Make NGrams lists and add example code
    As lists, NGrams can be of arbitary length.

--- Markov.hs
+++ Markov.hs
@@ -1,32 +1,28 @@
 module Markov (markovChain, generateTokens) where
 
 import qualified Data.Map as Map
-import System.Random (RandomGen, randomR)
+import Data.List (tails)
+import System.Random (RandomGen, randomR, getStdGen)
 
--- Example: 
--- :m + System.Random
--- let n = markovChain ["Tic", "Toc", "Tic", "Toc", "Tac"] Map.! ("Tic", "Toc")
--- take 20 $ generateTokens n (mkStdGen 42)
-
-type NGram a = (a, a)
+type NGram a = [a]
 data MarkovNode a = MarkovNode (NGram a) [MarkovNode a]
 type MarkovChain a = Map.Map (NGram a) (MarkovNode a)
 
-nGramMap :: Ord a => [a] -> Map.Map (NGram a) [NGram a]
-nGramMap = foldl addTransition Map.empty . toPairs . toNGrams
+nGramMap :: Ord a => Int -> [a] -> Map.Map (NGram a) [NGram a]
+nGramMap n = foldl addTransition Map.empty . toPairs . toNGrams n
   where
     addTransition m (from, to) = Map.insertWith (++) from [to] m
-    toNGrams = toPairs
-    toPairs l = zip l $ tail $ cycle l
+    toNGrams n = map (take n) . takeWhile ((>=n) . length) . tails
+    toPairs l = zip l $ tail l
 
-markovChain :: (Eq a, Ord a) => [a] -> MarkovChain a
-markovChain tokens = chain
+markovChain :: (Eq a, Ord a) => Int -> [a] -> MarkovChain a
+markovChain n tokens = chain
   where
-    chain = Map.mapWithKey toMarkovNode . nGramMap $ tokens
+    chain = Map.mapWithKey toMarkovNode . nGramMap n $ tokens
     toMarkovNode ngram = MarkovNode ngram . map (chain Map.!)
 
 getToken :: MarkovNode a -> a 
-getToken (MarkovNode ngram nodes) = fst ngram
+getToken (MarkovNode ngram nodes) = head ngram
 
 getNext :: RandomGen g => MarkovNode a -> g -> (MarkovNode a, g)
 getNext (MarkovNode ngram nodes) g = (nodes !! i, g')
@@ -39,3 +35,11 @@ generateTokens = go
     go n g = n' `seq` (getToken n : go n' g')
       where
         (n', g') = getNext n g
+
+generateSentence :: RandomGen g => MarkovChain String -> g -> String
+generateSentence c = unwords . dropWhile (('.' ==) . last) . take 20 . generateTokens (head $ Map.elems c)
+
+main = do
+  s <- readFile "data/poe.txt"
+  g <- getStdGen
+  print $ generateSentence (markovChain 2 $ words s) g
