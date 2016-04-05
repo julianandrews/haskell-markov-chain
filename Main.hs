278cdcc5e7d3ec2f3fa25d9e1e8081eb69143edb
Julian Andrews <jandrews@fusionbox.com>
2016-04-04 22:22:11 -0600

    Rework Main to user arguments

--- Main.hs
+++ Main.hs
@@ -1,36 +1,74 @@
+import Control.Monad (replicateM)
 import Control.Monad.Random (evalRandIO)
+import Data.List (nub)
+import Text.Printf (printf)
+import System.Console.GetOpt  -- (getOpt, ArgOrder(Permute), usageInfo, NoArg, Option)
+import System.Environment (getArgs)
+import System.Exit (exitSuccess, exitFailure)
+import System.IO (hPutStrLn, stderr)
 
-import Data.Markov (markovChain)
+import Data.Markov (markovChain, MarkovChain)
 import Data.Markov.Passphrase (cleanForPassphrase, passphrase)
--- import Data.Markov.Sentence (sentence)
 
-corpus = [
-    "corpus/bible.txt",
-    "corpus/shakespeare.txt",
-    "corpus/mountains_of_madness.txt",
-    "corpus/dunwich.txt",
-    "corpus/emma.txt",
-    "corpus/pride.txt",
-    "corpus/my_man_jeeves.txt",
-    "corpus/right_ho_jeeves.txt",
-    "corpus/holmes.txt"
+data Flag
+  = MinEntropy Double
+  | Number Int
+  | Suppress
+  | Help
+  deriving (Eq, Show)
+
+flags = [
+  Option
+    ['m']
+    ["min-entropy"]
+    (ReqArg (MinEntropy . read) "MINENTROPY")
+    "Minumum Entropy (defaults to 60)",
+  Option
+    ['n']
+    ["number"]
+    (ReqArg (Number . read) "NUMBER")
+    "Number of passphrases to generate",
+  Option
+    ['s']
+    ["suppress"]
+    (NoArg Suppress)
+    "Suppress entropy output",
+  Option
+    ['h']
+    ["help"]
+    (NoArg Help)
+    "Print this help message"
   ]
 
-concatFiles :: [FilePath] -> IO String
-concatFiles filenames = concat <$> mapM readFile filenames
+parse :: [String] -> IO (Double, Int, Bool, [String])
+parse argv = case getOpt Permute flags argv of
+  (args, files, []) ->
+      if Help `elem` args then hPutStrLn stderr usage >> exitSuccess
+      else do
+        let minEntropy = headOrDefault 60 [e | x@(MinEntropy e) <- args]
+        let number = headOrDefault 1 [n | x@(Number n) <- args]
+        let suppress = Suppress `elem` args
+        return (minEntropy, number, suppress, files)
+  (_, _, errs) -> hPutStrLn stderr (concat errs ++ usage) >> exitFailure
+  where usage = usageInfo "Usage: passphrase [OPTION]... [FILE]..." flags
 
-printPassphrase :: [FilePath] -> Double -> IO ()
-printPassphrase filenames minEntropy = do
-  chain <- markovChain 3 . cleanForPassphrase <$> concatFiles filenames
-  result <- evalRandIO $ passphrase minEntropy chain
-  print result
+headOrDefault :: a -> [a] -> a
+headOrDefault d [] = d
+headOrDefault _ (x:xs) = x
 
--- printSentece :: [FilePath] -> IO ()
--- printSentence filenames = do
---   chain <- markovChain 2 . words <$> concatFiles filenames
---   result <- evalRandIO $ sentence chain
---   print result
+genPassphrase :: Double -> String -> IO (String, Double)
+genPassphrase minEntropy = evalRandIO . passphrase minEntropy . markovChain 3
+
+getCorpus :: [String] -> IO String
+getCorpus [] = getContents
+getCorpus filenames = concat <$> mapM readFile filenames
 
 main :: IO ()
-main = printPassphrase corpus 64
--- main = printSentence corpus
+main = do
+  (minEntropy, number, suppress, files) <- getArgs >>= parse
+  string <- cleanForPassphrase <$> getCorpus files
+  passphrases <- replicateM number $ genPassphrase minEntropy string
+  mapM_ (printPassphrase suppress) passphrases
+  where
+    printPassphrase True = printf "%s\n" . fst
+    printPassphrase False = uncurry (printf "%s <%.2f>\n")
